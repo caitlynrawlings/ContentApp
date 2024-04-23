25941eab4a129e47c38f6041f11d92c31e4d9c21
wcm-khushi <65826614+wcm-khushi@users.noreply.github.com>
2024-04-23 10:34:33 -0700

    create content screen

---
+++ lib/content_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'image_widget.dart';
+
+class ContentScreen extends StatelessWidget {
+  final Map<String, dynamic> contentData;
+  final String language;
+
+  const ContentScreen({Key? key, required this.contentData, required this.language}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> contentWidgets = contentData['content'].map<Widget>((contentItem) {
+      if (contentItem['content-type'] == 'image') {
+        var imageData = contentItem['content'][language];
+        return ImageWidget(imagePath: imageData['path'], altText: imageData['alt'] ?? '');
+      } else {
+        var textData = contentItem['content'][language];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(textData),
+        );
+      }
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(contentData['title'][language] ?? 'Content Page'),
+      ),
+      body: ListView(children: contentWidgets),
+    );
+  }
+}

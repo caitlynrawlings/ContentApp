3b3370135a65e44e4612ed031bcfbc917d71cc92
mlin23 <mlin23@cs.washington.edu>
2024-04-24 15:19:07 -0700

    rename icon image files

--- parse/src/parser.py
+++ parse/src/parser.py
@@ -57,27 +57,25 @@ class Parser:
         return cell
 
     @staticmethod
-    def _parse_iconsubheading(cell, language, title, i):
+    def _parse_iconsubheading(cell, _, __, ___):
         newlines = cell.split('\n')
         name = Drive.get_file_name(link=newlines[0])
-        file_name = f"{title}-{i}-{language}-{name}"
-        path = os.path.join(UPDATE_DIR, file_name)
+        path = os.path.join(UPDATE_DIR, name)
 
         Drive.download_file_link(newlines[0], path)
         return {
-            "path": file_name,
+            "path": name,
             "subheading": newlines[1].strip()
         }
 
     @staticmethod
-    def _parse_callout(cell, language, title, i):
+    def _parse_callout(cell, _, __, ___):
         newlines = cell.split('\n')
         name = Drive.get_file_name(link=newlines[0])
-        file_name = f"{title}-{i}-{language}-{name}"
-        path = os.path.join(UPDATE_DIR, file_name)
+        path = os.path.join(UPDATE_DIR, name)
 
         Drive.download_file_link(newlines[0], path)
         return {
-            "path": file_name,
+            "path": name,
             "text": newlines[1].strip()
         }

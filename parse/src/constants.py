46ff7364630cbe7d750393ee7f7aea90318b430b
mlin23 <mlin23@cs.washington.edu>
2024-04-24 15:47:10 -0700

    ignore parsing demo sheet

--- parse/src/constants.py
+++ parse/src/constants.py
@@ -14,5 +14,5 @@ ALT_CONTENT = {"Image"}
 DOWNLOADABLE = {"Video"}.union(ALT_CONTENT)
 VALID_CONTENT_TYPES = {"Text", "Heading", "Subheading", "IconSubheading", "Callout", "Spacer"}.union(DOWNLOADABLE)
 
-SKIPPED_SHEETS = {"Languages", "Base"}
+SKIPPED_SHEETS = {"Languages", "Base", "Demo"}
 MAX_ROWS = 1000

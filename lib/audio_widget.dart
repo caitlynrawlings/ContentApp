dfd9bcea4a3700fe5958af3ce19779d7402b958a
wcm-khushi <65826614+wcm-khushi@users.noreply.github.com>
2024-04-29 10:39:07 -0700

    add audio widget

---
+++ lib/audio_widget.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+import 'main.dart'; 
+import 'package:just_audio/just_audio.dart';
+
+class AudioWidget extends StatefulWidget {
+  @override
+  final String audioAsset;
+  AudioWidget({Key? key, required this.audioAsset}) : super(key:key);
+  @override
+  State<AudioWidget> createState() => _AudioWidgetState();
+}
+
+class _AudioWidgetState extends State<AudioWidget> {
+  final AudioPlayer player1 = AudioPlayer();
+  bool isPlaying1 = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    //releasing resources
+    player1.dispose();
+  }
+
+  void _toggleAudio1() {
+    setState(() {
+      if (isPlaying1) {
+        player1.pause();
+      } else {
+        //set it
+        player1.setAsset(widget.audioAsset);
+        player1.play();
+      }
+      setState(() {
+        isPlaying1 = !isPlaying1; // Toggle play state
+      });
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  IconButton(
+      icon: Image.asset(
+        "assets/audio.png",
+        width: 60,
+        height: 60,
+      ),
+      onPressed: () => _toggleAudio1(),
+    );
+  }
+}

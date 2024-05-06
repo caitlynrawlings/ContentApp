51c715b339309209fcac8ec92bea59f861dc68de
wcm-khushi <65826614+wcm-khushi@users.noreply.github.com>
2024-05-06 13:59:03 -0700

    add toggle widget

---
+++ lib/toggle_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class ToggleWidget extends StatefulWidget {
+  final String title;
+  final String body;
+  const ToggleWidget({Key? key, required this.title, required this.body}) : super(key: key);
+
+  @override
+  _ToggleWidgetState createState() => _ToggleWidgetState();
+}
+
+class _ToggleWidgetState extends State<ToggleWidget> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _isExpanded = !_isExpanded; // Toggle the state
+            });
+          },
+          child: Row(
+            children: [
+              
+              AnimatedRotation(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                turns: _isExpanded ? 0.0 : -0.5,  
+                child: Icon(
+                  Icons.arrow_drop_down,  
+                  color: Theme.of(context).primaryColor,
+                  size: 24,
+                ),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  widget.title,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+        if (_isExpanded)
+          Padding(
+            padding: EdgeInsets.only(left: 10, top: 8),
+            child: Text(widget.body, style: TextStyle(fontSize: 16, color: Colors.black)),
+          ),
+      ],
+    );
+  }
+}
